0d780bda64af22103d98bfe9a6f19a63918c8370
JasonLiu001 <wangliu@isoftstone.com>
2018-06-17 15:06:41 +0800

    线上版本的tomcat端口为8080

--- src/main/java/cn/lands/liuwang/investservice/controller/BaseController.java
+++ src/main/java/cn/lands/liuwang/investservice/controller/BaseController.java
@@ -1,6 +1,9 @@
 package cn.lands.liuwang.investservice.controller;
 
+import cn.lands.liuwang.investservice.InvestServiceApplication;
 import cn.lands.liuwang.investservice.service.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class BaseController {
@@ -22,4 +25,6 @@ public class BaseController {
 
     @Autowired
     protected PlanInvestNumbersService planInvestNumbersService;
+
+    protected final Logger logger = LogManager.getLogger(this.getClass());
 }

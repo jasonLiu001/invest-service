e3947b65fef4c786a62de12843cb653c6dded60a
JasonLiu001 <wangliu@isoftstone.com>
2018-06-18 08:30:13 +0800

    修改tomcat默认实现

--- src/main/java/cn/lands/liuwang/investservice/InvestServiceApplication.java
+++ src/main/java/cn/lands/liuwang/investservice/InvestServiceApplication.java
@@ -4,10 +4,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "cn.lands.liuwang")
-public class InvestServiceApplication {
+public class InvestServiceApplication extends SpringBootServletInitializer {
     private static final Logger logger = LogManager.getLogger(InvestServiceApplication.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InvestServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InvestServiceApplication.class, args);

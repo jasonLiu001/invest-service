5f9f527874c00a7150a1a37f0ddbcb9eff7024f4
wangliu@isoftstone.com <liuwang@yiche.com>
2018-09-25 19:13:30 +0800

    添加vue支持

--- src/main/java/cn/lands/liuwang/investservice/config/WebSecurityConfig.java
+++ src/main/java/cn/lands/liuwang/investservice/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/lottery/**", "/css/**", "/js/**", "/images/**", "/web/about").permitAll()
+                .antMatchers("/", "/lottery/**", "/src/css/**", "/src/js/**", "/src/images/**", "/web/about").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()

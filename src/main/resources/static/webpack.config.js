5750a5047292dacb811a77b643829270b1a4994b
wangliu@isoftstone.com <liuwang@yiche.com>
2018-09-28 11:14:57 +0800

    打包文件修改

--- src/main/resources/static/webpack.config.js
+++ src/main/resources/static/webpack.config.js
@@ -1,12 +1,25 @@
 var path = require('path')
 var webpack = require('webpack')
 
+const APP_DIR = path.resolve(__dirname, './src');
+const MODULES_DIR = path.resolve(__dirname, './node_modules');
+
+const packageJson = require('./package.json');
+
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    main: './src/main.js',
+    vue: ['vue'],
+    vuetify: ['vuetify'],
+    vueRouter: ['vue-router'],
+    // vendor: Object.keys(packageJson.dependencies)
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
+    chunkFilename: '[name].js',
     publicPath: '/service/dist/',//这里的/service是tomcat的虚拟目录
-    filename: 'build.js'
+    filename: '[name].js',
+    // filename: 'build.js'
   },
   module: {
     rules: [
@@ -95,7 +108,8 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  //module.exports.devtool = '#source-map'
+  module.exports.devtool = 'false'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -103,8 +117,13 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
+    //提取公共的或者第三方类库的js到单独的文件
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vuetify', 'vueRouter', 'vue'],//这里面顺序注意，将公共引用的放到最后
+      // filename: 'vue.js',//这里的filename会覆盖output中的filename，可以在这为output输出一个新的文件名，可以省略
+    }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }

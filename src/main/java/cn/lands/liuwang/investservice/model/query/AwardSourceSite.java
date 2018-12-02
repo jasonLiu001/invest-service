66d38b05692013d9e12f0d879d4b93c70717681a
JasonLiu001 <wangliu@isoftstone.com>
2018-12-02 15:37:40 +0800

    添加更新站点

--- src/main/java/cn/lands/liuwang/investservice/model/query/AwardSourceSite.java
+++ src/main/java/cn/lands/liuwang/investservice/model/query/AwardSourceSite.java
@@ -2,6 +2,20 @@ package cn.lands.liuwang.investservice.model.query;
 
 /**
  * 更新奖号网站
+ *
+ * xml格式
+
+ 1. http://kaijiang.500.com/static/public/ssc/xml/qihaoxml/20181001.xml?_A=SRZANHID1538363085068【有历史数据】
+
+ JSON格式
+
+ 1. http://026012.com/index.php/index/index
+
+ 网页格式
+
+ 1. https://www.km28.com/lottery-gp/cqssc.html[最优]
+ 2. https://fx.cp2y.com/cqssckj/【2】
+ 3. https://kaijiang.aicai.com/cqssc/【3】
  */
 public class AwardSourceSite {
     /**
